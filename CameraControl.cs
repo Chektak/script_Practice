48396e00bf9363a132b33b805dc154b0c805db2b
Chektak <com123com11@naver.com>
2019-03-01 21:38:00 +0900

    aaaa

---
+++ CameraControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    /*Vector3 diff;
+    public GameObject target;
+    public float followSpeed=5;
+	// Use this for initialization
+	void Start () {
+        diff = target.transform.position - transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position - diff,
+            Time.deltaTime * followSpeed);
+	}*/
+    public GameObject target;
+    public float distance = 5f;
+
+    public float xTurnSpeed = 220.0f;
+    public float yTurnSpeed = 100.0f;
+
+    private float xPos = 0.0f;
+    private float yPos = 0.0f;
+
+    public float yPosMinLimit = -20f;
+    public float yPosMaxLimit = 80f;
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Vector3 angles = transform.eulerAngles;
+        xPos = angles.y;
+        yPos = angles.x;
+        Debug.Log("x : " + xPos + "," + "y : "+yPos);
+    }
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            distance -= 0.5f * Input.mouseScrollDelta.y;
+
+            if (distance < 0.5)
+                distance=1;
+            if (distance >= 10)
+                distance = 10;
+
+            xPos += Input.GetAxis("Mouse X") * xTurnSpeed * 0.02f;
+            yPos -= Input.GetAxis("Mouse Y") * yTurnSpeed * 0.02f;
+
+            yPos = ClampAngle(yPos, yPosMinLimit, yPosMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(yPos, xPos, 0);
+            Vector3 position = rotation * new Vector3(0, 0.0f, -distance) + 
+                target.transform.position+ new Vector3(0.0f, 0, 0.0f);
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 5f;
+    public Vector3 resetRotateValue = new Vector3();
+    private Vector3 position = new Vector3();
+    private bool isGround = true;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("w"))
+            position += new Vector3(0, 0, 1f);
+        if (Input.GetKeyDown("a"))
+            position= new Vector3(-1f, 0, 0);
+        if (Input.GetKeyDown("d"))
+            position = new Vector3(1f, 0, 0);
+        if (Input.GetKeyDown("s"))
+            position = new Vector3(0, 0, -1f);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            transform.rotation = Quaternion.Euler(resetRotateValue.x, resetRotateValue.y, resetRotateValue.z);
+        if (Input.GetKeyDown(KeyCode.Space)&&isGround==true)
+        {
+            position = new Vector3(0, 5, 0);
+            isGround = false;
+        }
+        transform.position += position * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGround = true;
+    }
+}
