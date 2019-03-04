bfe216303af60d4b7f8573d74fc89bf0461afc3d
Chektak <com123com11@naver.com>
2019-03-04 11:02:49 +0900

    eww

--- GUI/GUIControl.cs
+++ GUI/GUIControl.cs
@@ -6,12 +6,35 @@ using UnityEngine.UI;
 public class GUIControl : MonoBehaviour {
     public GameObject[] panels;
     public int openPanelNumber=0;
-	void Start () {
-		
-	}
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) && GameManager.isGamePlaying == false)
+        {
+            ChangeMainPanel();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && GameManager.isGamePlaying == true)
+        {
+            ChangePanel(3);
+            GameManager.ChangeControl(false);
+        }
+    }
 	
-	// Update is called once per frame
-	void ChangePanelNumber (int n) {
-        openPanelNumber = n;
+	/// <summary>
+    /// 패널을 바꾼다. 0:메인화면 1:옵션 2:크레딧 3:일시정지화면
+    /// </summary>
+    /// <param name="number"></param>
+	public void ChangePanel (int number) {
+        panels[openPanelNumber].SetActive(false);
+        openPanelNumber = number;
+        panels[openPanelNumber].SetActive(true);
 	}
+    
+    /// <summary>
+    /// 메인화면으로 이동한다.
+    /// </summary>
+    public void ChangeMainPanel() {
+        panels[openPanelNumber].SetActive(false);
+        openPanelNumber = 0;
+        panels[openPanelNumber].SetActive(true);
+    }
 }
--- GameManager.cs
+++ GameManager.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    public CameraControl mainCamera;
-    public Player player;
+    static public CameraControl mainCamera;
+    static public Player player;
     public GUIControl mainCanvas;
 
+    static public bool isGamePlaying = false;
     private void Awake()
     {
-        mainCamera.enabled = false;
-        player.enabled = false;
+        ChangeControl(false); 
+    }
+
+    /// <summary>
+    /// GUIControl에서 게임 일시정지와 Start버튼에서 사용한다.
+    /// </summary>
+    static public void GamePlay()
+    {
+        isGamePlaying = true;
+    }
+
+    /// <summary>
+    /// 게임플레이를 가능하게 할 지(카메라와 플레이어 이동) 설정한다.
+    /// </summary>
+    /// <param name="canPlay"></param>
+    static public void ChangeControl(bool canPlay) {
+        mainCamera.enabled = canPlay;
+        player.enabled = canPlay;
     }
 }
